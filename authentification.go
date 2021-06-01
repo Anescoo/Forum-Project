808e62d774eceb3a21a1932f5a4fcffbb203cfba
yann Bourgeron <75064765+Chakyu23@users.noreply.github.com>
2021-06-01 15:57:43 +0200

    Create authentification.go
    création du fichier, et de la première partie de la fonction d'enregistrement

---
+++ authentification.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+)
+
+func register(username string, password string, email string) {
+	fmt.Println(username, "  ", password, "  ", email)
+}
+
+func main() {
+	var username string
+	var password string
+	var email string
+	verifemail, _ := regexp.Compile("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,}")
+	majLetter, _ := regexp.Compile("[A-Z]")
+	minLetter, _ := regexp.Compile("[a-z]")
+	number, _ := regexp.Compile("[0-9]")
+	for i := 0; i < 1; i++ {
+		fmt.Println("entrez un username")
+		fmt.Scanln(&username)
+		if len(username) < 3 || len(username) > 15 {
+			i = -1
+		}
+
+	}
+	for e := 0; e < 1; e++ {
+		fmt.Println("entrez un mot de passe")
+		fmt.Scanln(&password)
+		if len(password) < 8 {
+			e = -1
+		}
+		if len(majLetter.FindAllStringSubmatchIndex(password, -1)) < 1 || len(minLetter.FindAllStringSubmatchIndex(password, -1)) < 2 || len(number.FindAllStringSubmatchIndex(password, -1)) < 2 {
+			e = -1
+		}
+	}
+	for u := 0; u < 1; u++ {
+		fmt.Println("entrez une email")
+		fmt.Scanln(&email)
+		if !verifemail.MatchString(email) {
+			u = -1
+		}
+	}
+	register(username, password, email)
+}
