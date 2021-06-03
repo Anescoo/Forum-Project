1f1aae30c4a54f4fde2c11d38678f4c5bfcf0422
yann Bourgeron <75064765+Chakyu23@users.noreply.github.com>
2021-06-03 08:54:06 +0200

    Update authentification.go
    ajout de la fonction de hash

--- authentification.go
+++ authentification.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
 	"regexp"
 )
@@ -9,6 +10,8 @@ func main() {
 	var username string
 	var password string
 	var email string
+	var retry bool = false
+	var retryLog string
 	verifemail, _ := regexp.Compile("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,}")
 	majLetter, _ := regexp.Compile("[A-Z]")
 	minLetter, _ := regexp.Compile("[a-z]")
@@ -17,7 +20,9 @@ func main() {
 		fmt.Println("entrez un username")
 		fmt.Scanln(&username)
 		if len(username) < 3 || len(username) > 15 {
-			i = -1 //retourné une érreur ici
+			i = -1
+			//retry = true
+			//retryLog = retryLog + "pseudo trop court,  "
 		}
 
 	}
@@ -25,18 +30,32 @@ func main() {
 		fmt.Println("entrez un mot de passe")
 		fmt.Scanln(&password)
 		if len(password) < 8 {
-			e = -1 //retourné une érreur ici
+			e = -1
+			//retry = true
+			//retryLog = retryLog + "mot de passe trop court,  "
 		}
 		if len(majLetter.FindAllStringSubmatchIndex(password, -1)) < 1 || len(minLetter.FindAllStringSubmatchIndex(password, -1)) < 2 || len(number.FindAllStringSubmatchIndex(password, -1)) < 2 {
-			e = -1 //retourné une érreur ici
+			e = -1
+			//retry = true
+			//retryLog = retryLog + "mot de passe trop faible,  "
 		}
 	}
 	for u := 0; u < 1; u++ { //retiré les for
 		fmt.Println("entrez une email")
 		fmt.Scanln(&email)
 		if !verifemail.MatchString(email) {
-			u = -1 //retourné une érreur ici
+			u = -1
+			//retry = true
+			//retryLog = retryLog + "email invalide,  "
 		}
 	}
-	fmt.Println(username, "  ", password, "  ", email)
+	if retry {
+		fmt.Println(retryLog)
+		//retourné une érreur ici
+	} else {
+		fmt.Println(username, "  ", password, "  ", email)
+		passwordHash := md5.Sum([]byte(password))
+		fmt.Println(username, "  ", passwordHash, "  ", email)
+	}
+
 }
