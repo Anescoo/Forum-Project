edb88797423d24090a47c3e681efa561022b827b
Louis Teilliais <louis.teilliais@ynov.com>
2021-06-07 08:51:49 +0200

    récupération données profil OK

--- src/go/connexion.go
+++ src/go/connexion.go
@@ -10,5 +10,16 @@ func Connexion(w http.ResponseWriter, req *http.Request){
 
 	t,_ := template.ParseFiles("./template/connexion.html", "./template/Header.html")
 	fmt.Print("Page de connexion ✔️ \n")
+
+	req.ParseForm()
+
+	
+	getPseudo := req.FormValue("pseudo")
+	getMdp := req.FormValue("mdp")
+
+	fmt.Println("Pseudo : " , getPseudo)
+	fmt.Println("Mot de Passe :", getMdp)
+
 	t.Execute(w, nil)
 }
+
