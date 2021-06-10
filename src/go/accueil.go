e5bb614111479be2d29162e502dac05f01dcba79
root <root@LAPTOP-5HIQ1AKJ.localdomain>
2021-06-10 11:23:51 +0200

    début création d'un post

--- src/go/accueil.go
+++ src/go/accueil.go
@@ -18,14 +18,23 @@ func Accueil(w http.ResponseWriter, req *http.Request) {
 	t, _ := template.ParseFiles("./template/Accueil.html", "./template/header.html")
 	fmt.Print("Page d'accueil ✔️ \n")
 
+	getPostValue := req.FormValue("PostValue")
+	fmt.Println(getPostValue)
 	bdd.MakeUser("Tao", "louis.teilliais@gmail.com", "Karim69lattrik")
-	bdd.MakePoste("Tao", "Ce post s'affiche dans la console", "test")
+	bdd.MakePoste("Tao",getPostValue,"test")
 	var arr []string
-	_, arr = bdd.GetPosteByID(1)
+	_, arr = bdd.GetPosteByID(2)
 	p := test {
 		UserName: arr[1],
 		Post: arr[2],
 	}
+
+	for i := 0; i < getAllPosts; i++ {
+		if getPostValue == bdd.GetPosteByID(i)[2] {
+			
+		}
+	}
+
 
 	if req.URL.Path == "/" { //verification de l'URL
 	} else if req.URL.Path != "/home" {
