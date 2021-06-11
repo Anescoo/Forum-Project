8865907b37ffb15405e83b22149ecd5e076caada
root <root@LAPTOP-5HIQ1AKJ.localdomain>
2021-06-11 10:52:21 +0200

    rebase

--- src/go/accueil.go
+++ src/go/accueil.go
@@ -1,20 +1,52 @@
 package handlers
 
 import (
-	"fmt"
-	"net/http"
-	"text/template"
+    "fmt"
+    "net/http"
+    "text/template"
+    bdd "../bdd"
 )
+
+type test struct {
+    UserName string
+    Post string
+    DatePoste string
+}
+
 
 func Accueil(w http.ResponseWriter, req *http.Request) {
 
-	t, _ := template.ParseFiles("./template/Accueil.html", "./template/header.html")
-	fmt.Print("Page d'accueil ✔️ \n")
+    t, _ := template.ParseFiles("./template/Accueil.html", "./template/header.html")
+    fmt.Print("Page d'accueil ✔️ \n")
 
-	if req.URL.Path == "/" { //verification de l'URL
-	} else if req.URL.Path != "/" {
-		http.Error(w, "404 not found", http.StatusNotFound)
-		return
-	}
-	t.Execute(w, nil)
+    getPostValue := req.FormValue("PostValue")
+    fmt.Println(getPostValue)
+    bdd.MakeUser("Tao", "louis.teilliais@gmail.com", "Karim69lattrik")
+    bdd.MakePoste("Tao",getPostValue,"test")
+    var arr []string
+    _, arr = bdd.GetPosteByID(2)    
+    p := test {
+        UserName: arr[1],
+        Post: arr[2],
+        DatePoste : arr[3],
+    }
+
+    // NbrPosts := bdd.GetAllPoste()
+
+    
+
+
+    // for i := 0; i <  ; i++ {
+    //     if getPostValue == bdd.GetPosteByID(i)[2] {
+
+    //     }
+    // }
+
+
+    if req.URL.Path == "/" { //verification de l'URL
+    } else if req.URL.Path != "/home" {
+        http.Error(w, "404 not found", http.StatusNotFound)
+        return
+    }
+    t.Execute(w, p)
 }
--- src/go/inscription.go
+++ src/go/inscription.go
@@ -1,44 +1,67 @@
 package handlers
 
 import (
-	"fmt"
-	"net/http"
-	"text/template"
+    "fmt"
+    "net/http"
+    "text/template"
+    
 
-	authent "./authent"
+    authent "./authent"
 )
 
 func Inscription(w http.ResponseWriter, req *http.Request) {
 
-	fmt.Print("Page d'inscription ✔️ \n")
+    fmt.Print("Page d'inscription ✔️ \n")
 
-	t, _ := template.ParseFiles("./template/inscription.html", "./template/header.html")
+    t, _ := template.ParseFiles("./template/inscription.html", "./template/header.html")
 
-	getPseudoInscription := req.FormValue("pseudoInscription")
-	getEmailInscription := req.FormValue("emailInscription")
-	getMdpInscription := req.FormValue("mdpInscription")
+    getPseudoInscription := req.FormValue("pseudoInscription")
+    getEmailInscription := req.FormValue("emailInscription")
+    getMdpInscription := req.FormValue("mdpInscription")
 
-	fmt.Println("Pseudo : ", getPseudoInscription)
-	fmt.Println("E-mail : ", getEmailInscription)
-	fmt.Println("Mot de passe : ", getMdpInscription)
+    fmt.Println("Pseudo : ", getPseudoInscription)
+    fmt.Println("E-mail : ", getEmailInscription)
+    fmt.Println("Mot de passe : ", getMdpInscription)
 
-	err := authent.Register(getPseudoInscription, getEmailInscription, getMdpInscription)
+    err := authent.Register(getPseudoInscription, getEmailInscription, getMdpInscription)
 
-	if err != 0 {
-		if err == 1 {
-			//mauvaise longueur pseudo
-		}
-		if err == 2 {
-			//mot de passe trop court
-		}
-		if err == 3 {
-			//mot de passe trop faible
-		}
-		if err == 4 {
-			//adresse email invalide
-		}
-	}
+    type HTMLData struct {
+        pseudoWrong string
+        mdpWrong string
+        mdpWeak string
+        emailInvalid string
+    }
 
-	t.Execute(w, nil)
+    htmlData := HTMLData{
+        pseudoWrong : "",
+        mdpWrong : "",
+        mdpWeak : "",
+        emailInvalid: "",
+    }
 
+    if err != 0 {
+        if err == 1 { 
+            
+            htmlData.pseudoWrong = "Votre pseudo est trop court"
+            fmt.Println("Votre pseudo est trop court")
+            //mauvaise longueur pseudo
+        
+        }else if err == 2 {
+            htmlData.mdpWrong = "Votre mot de passe est trop court"
+            fmt.Println("Votre mot de passe est trop court")
+            //mot de passe trop court
+        
+        }else if err == 3 {
+            htmlData.mdpWeak = "Votre mot de passe est trop faible"
+            fmt.Println("Votre mot de passe est trop faible")
+            //mot de passe trop faible
+        
+        }else if err == 4 {
+            htmlData.emailInvalid = "Adresse mail invalide"
+            fmt.Println("Adresse mail invalide")
+            //adresse email invalide
+        }
+    }
+    
+    t.Execute(w, nil)
 }
