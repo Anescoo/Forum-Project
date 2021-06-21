52162853a899eeae652011be30810b3907b45118
Louis Teilliais <louis.teilliais@ynov.com>
2021-06-21 08:31:48 +0200

    .

--- server.go
+++ server.go
@@ -7,20 +7,21 @@ import (
 	handlers "./src/go"
 )
 
-var ListeUserConnected []string
-var ListeUserKey []string
+// var ListeUserConnected []string
+// var ListeUserKey []string
 
-func ListImport(UserConnected string, userKey string) {
-	for i := 0; i < len(ListeUserConnected); i++ {
-		if ListeUserConnected[i] == UserConnected {
-			ListeUserKey[i] = UserConnected
-			return
-		}
-	}
+// func ListImport(UserConnected string, userKey string) {
+// 	for i := 0; i < len(ListeUserConnected); i++ {
+// 		if ListeUserConnected[i] == UserConnected {
+// 			ListeUserKey[i] = UserConnected
+// 			return
+// 		}
+// 	}
 
-	ListeUserConnected = append(ListeUserConnected, UserConnected)
-	ListeUserKey = append(ListeUserKey, userKey)
-}
+// 	ListeUserConnected = append(ListeUserConnected, UserConnected)
+// 	ListeUserKey = append(ListeUserKey, userKey)
+// }
+
 func main() {
 
     fmt.Print("Démarrage du serveur... 💬\n")
