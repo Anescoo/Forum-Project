ad166997c66de59380828109b43ea6008e94d88b
root <root@LAPTOP-5HIQ1AKJ.localdomain>
2021-06-22 16:44:51 +0200

    .

--- src/go/comms.go
+++ src/go/comms.go
@@ -4,6 +4,9 @@ import (
     "fmt"
     "net/http"
     "text/template"
+
+    // bdd "../bdd"
+
 )
 
 
@@ -13,7 +16,11 @@ func Comments(w http.ResponseWriter, req *http.Request) {
     t, _ := template.ParseFiles("./template/comms.html", "./template/header.html")
     fmt.Print("Page des commentaires post ✔️ \n")
     
-	
+    // var arr [][]string
+	// var posts []PostData
+    // _, test := bdd.GetPosteByID(1)
+    
+    // fmt.Println(IdToComment)
     
     t.Execute(w, nil)
 }
