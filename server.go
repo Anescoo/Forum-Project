4785ab83143281e09ddd9d438335244de952392d
Louis Teilliais <louis.teilliais@ynov.com>
2021-06-22 13:40:54 +0200

    Code commenté pour review

--- server.go
+++ server.go
@@ -29,11 +29,14 @@ func main() {
 
     http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
+
+    // Création des pages de notre site 
     http.HandleFunc("/home", handlers.Accueil)
     http.HandleFunc("/connexion", handlers.Connexion)
     http.HandleFunc("/profil", handlers.Profil)
     http.HandleFunc("/inscription", handlers.Inscription)
     http.HandleFunc("/userpost", handlers.UserPost)
 
+    // Choix du port que l'on sélectionne
     http.ListenAndServe(":8000", nil)
 }
