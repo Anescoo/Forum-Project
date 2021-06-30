25003d77f751da974487a95f5e1d709ea8452b8d
root <root@LAPTOP-5HIQ1AKJ.localdomain>
2021-06-30 12:01:33 +0200

    accueil fin

--- static/js/accueil.js
+++ static/js/accueil.js
@@ -1,4 +1,5 @@
 var textarea = document.getElementsByClassName("Comment-Zone") 
+const btn = document.getElementById("btn")
 
 function HideTextArea() {
     textarea.toggle("hidden")
@@ -7,4 +8,17 @@ function HideTextArea() {
 function TogglePopup(id) {
     const popup = document.getElementById(id)
     popup.toggleAttribute("hidden")
+    document.getElementById("popupForm").style.display="none";
+}
+
+
+
+function openForm() {
+    document.getElementById("popupForm").style.display="block";
+    btn.style.display="none"
+}
+
+function closeForm() {
+    document.getElementById("popupForm").style.display="none";
+    btn.style.display="flex"
 }
